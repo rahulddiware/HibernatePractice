985ce629979e477a7ccf2964e2094505dcb9f0e9
rahul <errahuldiware@gmail.com>
2018-04-19 16:28:49 +0530

    @DirtiesContext use to not affect to the previous test case if it using same data

--- SpringHibernate/src/test/java/com/rd/hibernate/SpringHibernate/CourseRepositoryTest.java
+++ SpringHibernate/src/test/java/com/rd/hibernate/SpringHibernate/CourseRepositoryTest.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.rd.hibernate.SpringHibernate.entity.Course;
@@ -22,11 +23,14 @@ public class CourseRepositoryTest {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	/*
-	 * @Test public void findById() { Course course =
-	 * repository.getCourseById(1001); assertEquals(1002, course.getId()); }
-	 */
 	@Test
+	public void findById() {
+		Course course = repository.getCourseById(1002);
+		assertEquals(1002, course.getId());
+	}
+
+	@Test
+	@DirtiesContext
 	public void deleteById() {
 		repository.deletById(1002);
 		assertNull(repository.getCourseById(1002));
